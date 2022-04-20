fb1f19362e62fbd81f91fb4262da162288071945
d940909492 <dong5455993@gmail.com>
2022-04-20 19:45:40 -0400

    updata array

---
+++ Data Structures/Array/Array_Simple_Example.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print(vector<int> &array){ //This is pass vector by reference, if pass by value, just remove "&" ;
+    for(int i = 0; i<array.size(); i++){
+        cout<<array.at(i)<<endl;
+    }
+}
+
+int main()
+{
+    vector<int>array = {1,2,3,4,5};
+    //print(array);
+    
+    array.push_back(6); //add an element after the array, this is O(1)
+    //print(array);
+
+    array.pop_back(); //remove last element, also O(1)
+    //print(array);
+    
+    array.emplace(array.begin() ,9); //insert an element in specific position, this is O(n) because it add an new element and move or shift rest all elements after it
+    print(array);
+    return 0;
+}
