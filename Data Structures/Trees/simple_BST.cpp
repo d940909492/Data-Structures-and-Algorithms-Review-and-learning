66c9af8823d895743e0238cdd8dba344d2171e32
ZhuoDong Zhen <88686053+d940909492@users.noreply.github.com>
2022-09-20 19:22:00 -0400

    still updating

---
+++ Data Structures/Trees/simple_BST.cpp
@@ -0,0 +1,22 @@
+/*
+binary search tree:
+Binary Search Trees are a non-linear data structure, it have 2 propertiesthe:
+left subtree of a node contains only nodes with keys lesser than the node’s key, while the right subtree of a node contains only nodes with keys greater than the node’s key.
+In most cases, the time complexity of operations on a BST, which include, lookups, insertions and deletions, take O(log N) time
+/
+
+#include<iostream>
+ 
+using namespace std;
+ 
+struct Node{
+  int data;
+  Node *left;
+  Node *right;
+};
+ 
+class BST{
+  
+
+
+}
