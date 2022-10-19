31c07472dab53fb85382e05d951916869c257c50
ZhuoDong Zhen <88686053+d940909492@users.noreply.github.com>
2022-10-19 17:28:10 -0400

    Update BFS.cpp

--- Algorithms/searching/BFS.cpp
+++ Algorithms/searching/BFS.cpp
@@ -1,1 +1,50 @@
+/*
+Breadth First Search For tree
 
+*/
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct Node {
+  int data;
+  Node *left;
+  Node *right;
+};
+
+Node *createNode(int data) {
+  Node *temp = new Node();
+  temp->data = data;
+  temp->left = nullptr;
+  temp->right = nullptr;
+  return temp;
+}
+
+void BFS(Node* root){
+  queue<Node*> q;
+   q.push(root);
+   while (q.empty() == false) {
+      Node *current = q.front();
+      cout << current->data <<endl;
+      q.pop();
+      if (current->left != NULL)
+         q.push(current->left);
+      if (current->right != NULL)
+         q.push(current->right);
+   }
+}
+
+int main() {
+  Node *root;
+  root = createNode(1);
+  root->left = createNode(2);
+  root->right = createNode(3);
+  root->left->left = createNode(4);
+  root->left->right = createNode(5);
+  root->right->left = createNode(6);
+  root->right->right = createNode(7);
+
+  BFS(root);
+  cin.get();
+}
